8b916769980ef4d630859df7f6861415c19aad5b
igortky <igortky@gmail.com>
2020-06-22 19:57:38 -0300

    update

--- src/app.js
+++ src/app.js
@@ -18,11 +18,7 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
   
-  /**
-   * Não existe a necessidade de armazenar o like  como uma variável no escopo global como 
-   * você colocou acima pode inseri-la diretamente "'set'ando" com o seu tipo no caso number.
-   * Lembre-se o like só existe dentro do Repositorio
-   */
+
   const repositorie = {
     id:uuid(),
     title,
@@ -46,20 +42,7 @@ app.put("/repositories/:id", (request, response) => {
 
   }
 
-  /**
-   * No desafio pede para modificar somente o title, techs e url, logo o 
-   * likes colocamos diretamente o que já tinhamos armazenado antes. já que não
-   * vamos buscar essa informação nem no params e nem no body.
-   * 
-   * E tente deixar as informações com um padrão:
-   * Exemplo:
-   * se você cria  um objeto ou array com as informações 1 2 3 4 5 logo na tela irá aparecer 
-   * 1 2 3 4 5, e se logo vc quer modificar  o numero 3 mas na hora de 'SETar' o obejto põe
-   * 3 2 1 5 4 na tela irá mudar tudo também... fica bem ruim de buscar um dado no olho.
-   * O computador da igual mas você quando olhar o Insomnia vai ficar meio WTF cade? então sempre
-   * mantenha a ordem inicial
-   *  
-   */
+
 
   const repositorie = {
     id,
@@ -92,7 +75,7 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params
 
-  //const newLike = likes+=1;
+ 
 
   const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
 
@@ -102,9 +85,7 @@ app.post("/repositories/:id/like", (request, response) => {
    repositories[repositorieIndex].likes = repositories[repositorieIndex].likes + 1;
 
 
-   /**
-    * Esqueceu de retornar a informação
-    */
+  
    return response.json(repositories[repositorieIndex]);
 });
 
